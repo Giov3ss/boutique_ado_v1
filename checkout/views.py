9e110a319b2f5bc7700ee8a8a09de283be702dc5
Giov3ss <giovani.sp25@gmail.com>
2023-06-17 19:04:09 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NK3jxCKyJfAqNZZPb1Yg1PAYMW27eJccm6UcequXGDEAZ3NUUHMfhmdDuEinuhz0WPOHI29NNbrg7jkZSWZjAhi00ain0vphR',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
